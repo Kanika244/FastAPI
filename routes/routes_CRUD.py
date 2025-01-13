c7b57b42aa94d8778109e086a977ec27063cf61d
Kanika Jain <157199143+Kanika244@users.noreply.github.com>
2025-01-13 14:45:34 +0530

    Update routes_CRUD.py

--- routes/routes_CRUD.py
+++ routes/routes_CRUD.py
@@ -12,19 +12,19 @@ async def create_student_route(student:StudentModel):
     return await create_student(student)
 
 @router.get("/students/{id}",response_model=StudentModel)
-async def show_student(id: str):
+async def show_student_route(id: str):
     return await show_student(id)
    
 
 
 
 @router.delete("/students/{id}",response_model=StudentModel)
-async def delete_student(id:str):
+async def delete_student_route(id:str):
     return await delete_student(id)
     
    
 @router.put("/students/{id}",response_model = StudentModel)
-async def update_student(id:str,student:StudentModel):
+async def update_student_route(id:str,student:StudentModel):
     update_data = student.model_dump(exclude_unset=True)
     return await update_student(id,update_data)
 
