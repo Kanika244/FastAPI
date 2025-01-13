86527dbb551c5bf64b9850429eed191d77bf0a89
Kanika Jain <157199143+Kanika244@users.noreply.github.com>
2025-01-13 14:25:51 +0530

    Update user_services.py

--- services/user_services.py
+++ services/user_services.py
@@ -1,9 +1,14 @@
+import jwt
 from database import user_collection
 from models import User
 from fastapi import HTTPException
 from passlib.context import CryptContext
+import datetime
 
 pwd_context = CryptContext(schemes=["bcrypt"],deprecated="auto")
+secret_key= "hello123"
+algorithm = "HS256"
+access_token_expire=30
  
 async def register_user(user:User):
      user_in_db=await user_collection.find_one({"email":user.email})
@@ -22,4 +27,14 @@ async def login_user(email:str,password:str):
      temp =  pwd_context.hash(password)
      if not user_in_db or pwd_context.verify(temp,user_in_db["password"]):
         raise HTTPException(status_code=401,detail="Invalid login or password")
-     return {"message":"Login Successful","User":User(**user_in_db)}
+     
+     access_token = create_access_token(data={"email":user_in_db["email"]})
+     return {"message":"Login Successful","access_token":access_token}
+
+def create_access_token(data:dict):
+    to_encode  = data.copy()
+    
+    expire = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes = access_token_expire)
+    to_encode.update({"exp":expire})
+    encode_jwt = jwt.encode(to_encode,secret_key,algorithm=algorithm)
+    return encode_jwt
