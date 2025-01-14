b4a03b61033f8051d39db70e89a9c01e57f7b233
Kanika Jain <157199143+Kanika244@users.noreply.github.com>
2025-01-14 22:17:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI
-from routes.routes_CRUD import router
 from routes.user_routes import router as user_router
-
+from routes.routes_CRUD import router
 
 app = FastAPI()
 
 
 
-app.include_router(router, prefix="/api")
-for route in app.routes:
-    print(f"Path:{route.path},Method:{route.methods}")
+app.include_router(router,prefix="/api")
 
 app.include_router(user_router,prefix="/api")
+#app.include_router(router_auth,prefix="/api")
