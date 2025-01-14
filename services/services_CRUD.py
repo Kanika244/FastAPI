4098dcfc1485981721575a6896a794f2671f6fe2
Kanika Jain <157199143+Kanika244@users.noreply.github.com>
2025-01-14 12:14:08 +0530

    Update services_CRUD.py

--- services/services_CRUD.py
+++ services/services_CRUD.py
@@ -1,40 +1,60 @@
-from fastapi import  HTTPException
-from models import StudentModel
+from bson.errors import InvalidId
+from fastapi import HTTPException, Depends
+from starlette.responses import JSONResponse
+
+from models import StudentModel, User
 from database import collection
 from bson import ObjectId
 
 
 
-async def create_student(student:StudentModel):
+
+
+async def create_student(student: StudentModel):
     new_student = student.model_dump(exclude=["id"])
     result = await collection.insert_one(new_student)
-    new_student['id']=str(result.inserted_id)
-    return StudentModel(**new_student)
+    new_student['id'] = str(result.inserted_id)
+    return new_student
 
 
 async def show_student(id: str):
-        student = await collection.find_one({"_id": ObjectId(id)})
-        
-        if not student:
-            raise HTTPException(status_code=404, detail=f"Student {id} not found")
 
-        student["id"] = str(student["_id"])
-        del student["_id"]
-        return StudentModel(**student)
-   
+    try:
+        object_id = ObjectId(id)
+    except InvalidId:
+        raise HTTPException(status_code=400, detail="Invalid ID format")
 
-    
-async def delete_student(id:str):
-    
+    student = await collection.find_one({"_id": ObjectId(id)})
+
+    if not student:
+        raise HTTPException(status_code=404, detail=f"Student {id} not found")
+
+    student["id"] = str(student["_id"])
+    del student["_id"]
+    return  StudentModel(**student)
+
+
+
+async def delete_student(id: str) -> JSONResponse:
+    try:
+        object_id = ObjectId(id)
+    except InvalidId:
+        raise HTTPException(status_code=400, detail="Invalid ID format")
     delete_result = await collection.delete_one({"_id": ObjectId(id)})
     if delete_result.deleted_count == 0:
-        raise HTTPException(status_code=404,detail="Item not found")
-    return {"message":"Item deleted successfully"}
+        raise HTTPException(status_code=404, detail="Item not found")
 
 
+    return JSONResponse(content={"message": "Item deleted successfully"})
 
-async def update_student(id:str,student:StudentModel):
-    updated_item = student.model_dump(exclude=["id"])
+
+async def update_student(id: str, student: StudentModel):
+    if isinstance(student, dict):
+        student = StudentModel(**student)
+    updated_item = {
+        key: getattr(student, key)
+        for key in student.__annotations__ if key != "id"
+    }
     result = await collection.update_one(
         {"_id": ObjectId(id)},
         {"$set": updated_item}
